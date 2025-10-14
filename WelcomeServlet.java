83c11d60c49bf4a3eb944af3c6d545443c7242de
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-10-14 19:10:16 +0530

    Create WelcomeServlet when login successful

---
+++ WelcomeServlet.java
@@ -0,0 +1,28 @@
+package demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/Welcome")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String n = request.getParameter("username");
+		out.print("Welcome" +n);
+		// TODO Auto-generated method stub
+	}
+
+}
